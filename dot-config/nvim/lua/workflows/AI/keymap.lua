2295d1961386f1c0b1860f1f8d4eccce3bc540f2
sangyeon kim <greyhairchooselife@gmail.com>
2025-01-23 01:12:57 +0900

    keymap: dismiss copilot

--- dot-config/nvim/lua/workflows/AI/keymap.lua
+++ dot-config/nvim/lua/workflows/AI/keymap.lua
@@ -1,9 +1,17 @@
 local map = vim.keymap.set
 local opt = { noremap = true, silent = true }
 
+-- >>>>>>>>>>>>>>>>> context provider
 map("n", "<leader>y", Save_entire_buffer_to_register_for_AI_prompt, opt)
 map("v", "<leader>y", Save_visual_selection_to_register_for_AI_prompt, opt)
+-- >>>>>>>>>>>>>>>>> context provider
 
-map("i", "<A-Down>", "<Plug>(copilot-accept-line)", opt)
+-- >>>>>>>>>>>>>>>>> Avante
 map("n", "<leader>aa", "<cmd>AvanteChat<cr>", opt)
 map("n", "<leader>af", "<cmd>AvanteFocus<cr>", opt)
+-- >>>>>>>>>>>>>>>>> Avante
+
+-- >>>>>>>>>>>>>>>>> Copilot
+map("i", "<A-Down>", "<Plug>(copilot-accept-line)", opt)
+map("i", "<C-e>", "<Plug>(copilot-dismiss)", opt)
+-- <<<<<<<<<<<<<<<<< Copilot
