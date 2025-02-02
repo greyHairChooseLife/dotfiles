1e1afaf6f4a6743bf0bb9aa263abb16d87b7a274
sangyeon kim <greyhairchooselife@gmail.com>
2025-02-02 22:21:35 +0900

    nvim: remove etc

--- dot-config/nvim/lua/workflows/AI/plugins.lua
+++ dot-config/nvim/lua/workflows/AI/plugins.lua
@@ -49,7 +49,6 @@ return {
 			require("avante_lib").load()
 			require("avante").setup({
 				---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
-				-- provider = "claude", -- Recommend using Claude
 				provider = "claude", -- Recommend using Claude
 				auto_suggestions_provider = "claude", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
 				-- claude = {
