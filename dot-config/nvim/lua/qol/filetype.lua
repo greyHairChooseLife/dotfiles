aafc97a3cea04ae24b6e976a6fda5b4766ba8ab6
sangyeon kim <greyhairchooselife@gmail.com>
2025-02-03 14:55:32 +0900

    nvim: .md에서 tabwidth 2칸

--- dot-config/nvim/lua/qol/filetype.lua
+++ dot-config/nvim/lua/qol/filetype.lua
@@ -29,3 +29,13 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.fn.settabvar(tabnr, "tabname", "GV") -- GV에 탭이름 변경
 	end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "markdown",
+	callback = function()
+		vim.opt.tabstop = 2 -- 탭을 2칸으로 설정
+		vim.opt.shiftwidth = 2 -- 자동 들여쓰기 2칸
+		vim.opt.softtabstop = 2 -- 백스페이스로 2칸씩 지우기
+		vim.opt.expandtab = true -- 탭을 공백으로 변환
+	end,
+})
