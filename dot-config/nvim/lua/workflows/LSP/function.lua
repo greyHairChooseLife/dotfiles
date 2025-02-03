db9ecf05ead5f7502d935b954b851d60b96cb93f
sangyeon kim <greyhairchooselife@gmail.com>
2025-02-03 10:21:55 +0900

    nvim: etc

--- dot-config/nvim/lua/workflows/LSP/function.lua
+++ dot-config/nvim/lua/workflows/LSP/function.lua
@@ -1,9 +1,13 @@
+local virtual_text_config = {
+	prefix = " ",
+}
 local virtual_text_enabled = true
 
 function ToggleVirtualText()
 	virtual_text_enabled = not virtual_text_enabled
+
 	vim.diagnostic.config({
-		virtual_text = virtual_text_enabled,
+		virtual_text = virtual_text_enabled and virtual_text_config or false,
 		underline = true,
 		signs = false, -- sign column에 아이콘 표시
 		update_in_insert = false, -- 입력 모드 중 업데이트 비활성화
