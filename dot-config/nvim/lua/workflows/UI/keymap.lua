31ec0d9bb69d3c5acb912497383a1bd1804f608e
sangyeon kim <greyhairchooselife@gmail.com>
2025-02-03 14:08:52 +0900

    nvim: misc keymap

--- dot-config/nvim/lua/workflows/UI/keymap.lua
+++ dot-config/nvim/lua/workflows/UI/keymap.lua
@@ -2,6 +2,8 @@ local map = vim.keymap.set
 local opt = { noremap = true, silent = true }
 
 -- >>>>>>>>>>>>>>>>>>>> Noice.nvim
-map("n", "<Space>n", "<cmd>NoiceDismiss<CR>")
+-- DEPRECATED:: 2025-02-03
+-- map("n", "<Space>n", "<cmd>NoiceDismiss<CR>")
+
 map("n", ",.N", "<cmd>Noice telescope<CR>") -- N for Noice
 -- <<<<<<<<<<<<<<<<<<<<
