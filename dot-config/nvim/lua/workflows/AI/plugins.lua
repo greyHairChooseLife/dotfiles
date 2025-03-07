aa46838fbfa8e275e5360741f990089e27a9f1a6
sangyeon kim <greyhairchooselife@gmail.com>
2025-03-08 08:19:14 +0900

    feat(nvim): add CopilotChat plugin and update locks
    - Added `CopilotChat.nvim` plugin with necessary commands and dependencies.

--- dot-config/nvim/lua/workflows/AI/plugins.lua
+++ dot-config/nvim/lua/workflows/AI/plugins.lua
@@ -141,4 +141,32 @@ return {
 			})
 		end,
 	},
+
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		cmd = {
+			"CopilotChat",
+			"CopilotChatOpen",
+			"CopilotChatClose",
+			"CopilotChatToggle",
+			"CopilotChatStop",
+			"CopilotChatReset",
+			"CopilotChatSave",
+			"CopilotChatLoad",
+			"CopilotChatPrompts",
+			"CopilotChatModels",
+			"CopilotChatAgents",
+			"CopilotChatCommit",
+			-- "CopilotChat<PromptName>"
+		},
+		dependencies = {
+			{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+			-- See Configuration section for options
+		},
+		-- See Commands section for default commands if you want to lazy load on them
+	},
 }
