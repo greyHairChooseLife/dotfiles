6e9e4d18a3479d5b8f65ab71f4bb97370b8c615e
sangyeon kim <greyhairchooselife@gmail.com>
2025-03-14 21:32:28 +0900

    refactor(keybind): simplify leap motion mappings
    - gj/gk로 leap 동작하도록 단순화
    - 타이머 기반 키 맵핑 로직 제거
    - gg 등 기존 g 접두 명령어들과의 충돌 방지

--- dot-config/nvim/lua/qol/leap.lua
+++ dot-config/nvim/lua/qol/leap.lua
@@ -17,49 +17,53 @@ local function execute_leap_backward()
 	vim.api.nvim_feedkeys(leap_backward, "n", false)
 end
 
-map({ "n", "v" }, "g", function()
-	-- set g_pressed flag and start timer
-	g_pressed = true
-	if timer then
-		timer:stop()
-	end
-	timer = vim.defer_fn(function()
-		if g_pressed then
-			-- If no 'j' or 'k' follows, do the normal 'g' command
-			vim.api.nvim_feedkeys("g", "n", true)
-			g_pressed = false
-		end
-	end, TIME)
-end, {})
+-- MEMO:: 당초 jk/kj에 맵핑하느라 생각해낸 방식이고, 아래처럼 시도하면 'gg'를 쓰기 힘들다.
 
-map({ "n", "v" }, "j", function()
-	if g_pressed then
-		-- g was pressed before j, trigger leap-forward
-		g_pressed = false
-		if timer then
-			timer:stop()
-			timer = nil
-		end
-		execute_leap_forward()
-	else
-		-- do the normal 'j' movement
-		vim.api.nvim_feedkeys("j", "n", true)
-	end
-end, {})
+-- map({ "n", "v" }, "g", function()
+-- 	-- set g_pressed flag and start timer
+-- 	g_pressed = true
+-- 	if timer then
+-- 		timer:stop()
+-- 	end
+-- 	timer = vim.defer_fn(function()
+-- 		if g_pressed then
+-- 			-- If no 'j' or 'k' follows, do the normal 'g' command
+-- 			vim.api.nvim_feedkeys("g", "n", true)
+-- 			g_pressed = false
+-- 		end
+-- 	end, TIME)
+-- end, {})
 
-map({ "n", "v" }, "k", function()
-	if g_pressed then
-		-- g was pressed before k, trigger leap-backward
-		g_pressed = false
-		if timer then
-			timer:stop()
-			timer = nil
-		end
-		execute_leap_backward()
-	else
-		-- do the normal 'k' movement
-		vim.api.nvim_feedkeys("k", "n", true)
-	end
-end, {})
+-- map({ "n", "v" }, "j", function()
+-- 	if g_pressed then
+-- 		-- g was pressed before j, trigger leap-forward
+-- 		g_pressed = false
+-- 		if timer then
+-- 			timer:stop()
+-- 			timer = nil
+-- 		end
+-- 		execute_leap_forward()
+-- 	else
+-- 		-- do the normal 'j' movement
+-- 		vim.api.nvim_feedkeys("j", "n", true)
+-- 	end
+-- end, {})
 
+-- map({ "n", "v" }, "k", function()
+-- 	if g_pressed then
+-- 		-- g was pressed before k, trigger leap-backward
+-- 		g_pressed = false
+-- 		if timer then
+-- 			timer:stop()
+-- 			timer = nil
+-- 		end
+-- 		execute_leap_backward()
+-- 	else
+-- 		-- do the normal 'k' movement
+-- 		vim.api.nvim_feedkeys("k", "n", true)
+-- 	end
+-- end, {})
+
+map({ "n", "v" }, "gj", "<Plug>(leap-forward)")
+map({ "n", "v" }, "gk", "<Plug>(leap-backward)")
 map({ "n" }, ",l", "<Plug>(leap-from-window)")
