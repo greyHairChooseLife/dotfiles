5b5d39b048b946c8885cdfcb1e88367c67432fb7
sangyeon kim <greyhairchooselife@gmail.com>
2025-03-25 23:33:28 +0900

    feat(csv): enhance CSV viewer with sticky header and wrap options
    - CSV 뷰어에 고정 헤더 기능 추가
    - 수평 스크롤을 위한 줄바꿈 비활성화 설정
    - 헤더 구분자 스타일링 옵션 구성

--- dot-config/nvim/lua/workflows/big_data/filetype.lua
+++ dot-config/nvim/lua/workflows/big_data/filetype.lua
@@ -4,5 +4,6 @@ vim.api.nvim_create_autocmd("FileType", {
 		require("csvview").enable()
 
 		vim.opt_local.mousescroll = "ver:3,hor:6" -- Enable horizontal scroll
+		vim.opt_local.wrap = false
 	end,
 })
--- dot-config/nvim/lua/workflows/big_data/plugins.lua
+++ dot-config/nvim/lua/workflows/big_data/plugins.lua
@@ -7,6 +7,24 @@ return {
 		opts = {
 			view = {
 				display_mode = "border",
+				--- The line number of the header
+				--- If this is set, the line is treated as a header. and used for sticky header feature.
+				--- see also: `view.sticky_header`
+				--- @type integer|false
+				header_lnum = 1,
+
+				--- The sticky header feature settings
+				--- If `view.header_lnum` is set, the header line is displayed at the top of the window.
+				sticky_header = {
+					--- Whether to enable the sticky header feature
+					--- @type boolean
+					enabled = true,
+
+					--- The separator character for the sticky header window
+					--- set `false` to disable the separator
+					--- @type string|false
+					separator = "",
+				},
 			},
 		},
 
