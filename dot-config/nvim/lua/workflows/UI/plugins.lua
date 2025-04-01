7cb799770474fcaed5c21a65cdd8e5d0422bf819
sangyeon kim <greyhairchooselife@gmail.com>
2025-04-02 00:47:48 +0900

    chore(deps): update packages and adjust noice.nvim config
    - 여러 플러그인 버전 업데이트
    - noice.nvim 의존성 설정 위치 상단으로 이동
    - noice.nvim cmdline 뷰를 팝업에서 클래식 스타일로 변경

--- dot-config/nvim/lua/workflows/UI/plugins.lua
+++ dot-config/nvim/lua/workflows/UI/plugins.lua
@@ -723,6 +723,14 @@ return {
 	{
 		"folke/noice.nvim",
 		event = "VeryLazy",
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
 		opts = {
 			lsp = {
 				progress = {
@@ -751,7 +759,7 @@ return {
 			},
 			cmdline = {
 				enabled = true, -- enables the Noice cmdline UI
-				view = "cmdline_popup", -- view for rendering the cmdline. Set `cmdline` to get a classic cmdline at the bottom or try 'cmdline_popup'
+				view = "cmdline", -- view for rendering the cmdline. Set `cmdline` to get a classic cmdline at the bottom or try 'cmdline_popup'
 				opts = {}, -- global options for the cmdline. See section on views
 				---@type table<string, CmdlineFormat>
 				format = {
@@ -793,14 +801,6 @@ return {
 					},
 				},
 			},
-		},
-		dependencies = {
-			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-			"MunifTanjim/nui.nvim",
-			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
-			"rcarriga/nvim-notify",
 		},
 	},
 }
