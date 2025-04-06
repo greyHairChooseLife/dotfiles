c22e484541a408666b6167e774f2bb527577a2c6
sangyeon kim <greyhairchooselife@gmail.com>
2025-04-06 22:26:38 +0900

    feat(neovim): vimResized autcmd로 layout resize

--- dot-config/nvim/lua/workflows/UI/auto_cmd.lua
+++ dot-config/nvim/lua/workflows/UI/auto_cmd.lua
@@ -15,3 +15,11 @@ vim.api.nvim_create_autocmd("BufLeave", {
 		end
 	end,
 })
+
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+	callback = function()
+		-- MEMO:: 현재의 fix를 해둬도, vim process의 gui 자체가 작아져버리면 해당 사이즈로 고정되어버린다.
+		-- edge.nvim 이었나? 장기적으로는 거기로 옮겨가는게 좋을듯
+		ReloadLayout()
+	end,
+})
