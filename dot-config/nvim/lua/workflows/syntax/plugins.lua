e1df5842904d561405ec9a4d9d094cab6cc2f1a7
sangyeon kim <greyhairchooselife@gmail.com>
2025-04-16 11:40:22 +0900

    feat(nvim): treesitter extend textobject of function, param, loop ...

--- dot-config/nvim/lua/workflows/syntax/plugins.lua
+++ dot-config/nvim/lua/workflows/syntax/plugins.lua
@@ -6,6 +6,7 @@ return {
 		build = ":TSUpdate",
 		event = { "BufReadPre", "BufNewFile" },
 		lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+		dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
 		init = function(plugin)
 			-- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
 			-- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
@@ -66,6 +67,25 @@ return {
 				},
 			},
 			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+					keymaps = {
+						-- You can use the capture groups defined in textobjects.scm
+						["ia"] = "@parameter.inner",
+						["aa"] = "@parameter.outer",
+						["ii"] = "@conditional.inner",
+						["ai"] = "@conditional.outer",
+						["il"] = "@loop.inner",
+						["al"] = "@loop.outer",
+						["ic"] = "@class.inner",
+						["ac"] = "@class.outer",
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["it"] = "@comment.inner",
+						["at"] = "@comment.outer",
+					},
+				},
 				move = {
 					enable = true,
 					goto_next_start = {
@@ -73,7 +93,11 @@ return {
 						["]c"] = "@class.outer",
 						["]a"] = "@parameter.inner",
 					},
-					goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+					goto_next_end = {
+						["]F"] = "@function.outer",
+						["]C"] = "@class.outer",
+						["]A"] = "@parameter.inner",
+					},
 					goto_previous_start = {
 						["[f"] = "@function.outer",
 						["[c"] = "@class.outer",
