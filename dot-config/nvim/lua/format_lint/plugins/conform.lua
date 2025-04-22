613dcf2a075cfb080ac1340964642b0fb274cfb4
sangyeon kim <greyhairchooselife@gmail.com>
2025-04-22 22:49:33 +0900

    feat(nvim): lint 플러그인 설치 및 python format 세팅 개선

--- dot-config/nvim/lua/format_lint/plugins/conform.lua
+++ dot-config/nvim/lua/format_lint/plugins/conform.lua
@@ -5,12 +5,24 @@ return {
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua", stop_after_first = true },
+				http = { "kulala" },
 				javascript = { "prettierd", "prettier", stop_after_first = true },
 				typescript = { "prettierd", "prettier", stop_after_first = true },
 				javascriptreact = { "prettierd", "prettier", stop_after_first = true },
 				typescriptreact = { "prettierd", "prettier", stop_after_first = true },
-				json = { "prettier", stop_after_first = true },
-				python = { "ruff", stop_after_first = true },
+				json = { "prettierd", stop_after_first = true },
+				python = { "ruff_organize_imports", "ruff_fix", "ruff_format", stop_after_first = true },
+				bash = { "shfmt" },
+				sh = { "shfmt" },
+				yaml = { "yamlfmt" },
+				toml = { "taplo" },
+			},
+			formatters = {
+				kulala = {
+					command = "kulala-fmt",
+					args = { "format", "$FILENAME" },
+					stdin = false,
+				},
 			},
 			format_on_save = {
 				timeout_ms = 500,
@@ -18,41 +30,25 @@ return {
 			},
 		})
 	end,
-}
+	keys = {
+		-- {
+		-- 	"<leader>rf",
+		-- 	function()
+		-- 		local bufnr = vim.api.nvim_get_current_buf()
+		-- 		local filetype = vim.bo[bufnr].filetype
+		-- 		local formatters = require("conform").list_formatters_for_buffer(bufnr)
 
--- REF: 고수의 설정인듯 하니 mason source를 참고하자.
--- return {
---   "stevearc/conform.nvim",
---   opts = {},
---   config = function()
---     require("conform").setup({
---       format_on_save = nil,
---       formatters_by_ft = {
---         lua = { "stylua" },
---         javascript = { "biome", "prettierd", "prettier", stop_after_first = true },
---         typescript = { "biome", "prettierd", "prettier", stop_after_first = true },
---         javascriptreact = { "biome", "prettierd", "prettier", stop_after_first = true },
---         typescriptreact = { "biome", "prettierd", "prettier", stop_after_first = true },
---         vue = { "biome", "prettierd", "prettier", stop_after_first = true },
---         css = { "biome", "prettierd", "prettier", stop_after_first = true },
---         html = { "prettierd", "prettier", stop_after_first = true },
---         go = { "gofmt" },
---         bash = { "shfmt" },
---         sh = { "shfmt" },
---         yaml = { "yamlfmt" },
---         xml = { "xmlformat" },
---         json = { "biome", "jq", stop_after_first = true },
---         toml = { "taplo" },
---       },
---     })
---   end,
---   keys = {
---     {
---       "<leader>rf",
---       function()
---         require("conform").format({ lsp_fallback = true })
---       end,
---       desc = "Format",
---     },
---   },
--- }
+		-- 		local formatted = require("conform").format({
+		-- 			bufnr = bufnr,
+		-- 			lsp_fallback = true,
+		-- 			async = false,
+		-- 		})
+
+		-- 		vim.notify("Filetype: " .. filetype)
+		-- 		vim.notify("Available formatters: " .. vim.inspect(formatters))
+		-- 		vim.notify("Formatting result: " .. tostring(formatted))
+		-- 	end,
+		-- 	desc = "Format with debug info",
+		-- },
+	},
+}
