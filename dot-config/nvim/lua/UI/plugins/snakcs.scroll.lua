4d2472ec01e84cb566346544597806d9335bcebd
sangyeon kim <greyhairchooselife@gmail.com>
2025-04-29 09:52:14 +0900

    plugin: snacks.nvim

---
+++ dot-config/nvim/lua/UI/plugins/snakcs.scroll.lua
@@ -0,0 +1,15 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+		scroll = {
+			enabled = true,
+			animate = {
+				duration = { step = 15, total = 100 },
+				easing = "linear",
+			},
+		},
+	},
+}
