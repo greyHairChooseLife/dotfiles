da5857f112a6a77a2b8372f5ad80ad6a5a60b4e2
sangyeon kim <greyhairchooselife@gmail.com>
2025-08-20 21:49:54 +0900

    feat: fzf with git

---
+++ dot-config/bash.sub/git_fzf_integration.sh
@@ -0,0 +1,108 @@
+# MEMO:: checkout git branch/tag, with a preview showing the commits between the tag/branch and HEAD
+look_branch_or_tag() {
+    local tags branches target
+    local header="<Enter>: checkout to the Ref & detach, <C-e>: Ref cp & abort, <A-e>: Ref cp"
+
+    branches=$(
+        git --no-pager branch --all \
+            --format="%(if)%(HEAD)%(then)%(else)%(if:equals=HEAD)%(refname:strip=3)%(then)%(else)%1B[0;34;1mbranch%09%1B[m%(refname:short)%(end)%(end)" \
+            | sed '/^$/d'
+    ) || return
+
+    tags=$(
+        git --no-pager tag | awk '{print "\x1b[35;1mtag\x1b[m\t" $1}'
+    ) || return
+
+    target=$(
+        (   
+            echo "$branches"
+            echo "$tags"
+        ) \
+            | fzf \
+                --no-hscroll --no-multi -n 2 --ansi \
+                --header="$header" \
+                --preview="git --no-pager log -150 --pretty=format:%s '..{2}'" \
+                --bind 'ctrl-e:execute(printf "%s" {2} | xclip -selection clipboard)+abort' \
+                --bind 'alt-e:execute(printf "%s" {2} | xclip -selection clipboard)'
+    ) || return
+
+    git checkout $(awk '{print $2}' <<< "$target")
+}
+
+alias git_log_no_graph='git log --color=always --format="%C(auto)%h%d %s    %C(brightblack dim italic)%cr% C(ul)%an%C(reset) " "$@"'
+_gitLogLineToHash="echo {} | grep -o '[a-f0-9]\{7\}' | head -1"
+_viewGitLogLine="$_gitLogLineToHash | xargs -I % sh -c 'git show --color=always %'"
+
+# MEMO:: checkout git commit with previews
+look_commit() {
+    local commit
+
+    local header="<Enter>: show commit with delta, <C-e>: Hash cp & abort, <A-e>: Hash cp"
+
+    commit=$(
+        git_log_no_graph \
+            | fzf \
+                --no-sort --tiebreak=index --no-multi --ansi \
+                --header="$header" \
+                --preview="$_viewGitLogLine" \
+                --bind "ctrl-e:execute($_gitLogLineToHash | xclip -selection clipboard)+abort" \
+                --bind "alt-e:execute($_gitLogLineToHash | xclip -selection clipboard)"
+    )
+
+    git log -p -1 $(echo "$commit" | sed "s/ .*//") | delta
+}
+
+look_graph_log() {
+    local command_list=$(
+        cat << EOF
+gls   /  git log --oneline --graph                 --all  --decorate --simplify-by-decoration
+glg   /  git log --oneline --graph --pretty=medium --stat
+glga  /  git log --oneline --graph --pretty=medium --all
+glgo  /  git log --oneline --graph
+glgao /  git log --oneline --graph                 --all
+glgP  /  git log --oneline --graph --pretty=medium origin/HEAD..
+EOF
+    )
+
+    _extract_git_command() {
+        echo "$1" | sed 's/^[^ ]* - //'
+    }
+
+    local header="<Enter>: run the command"
+
+    local cmd=$(echo "$command_list" | fzf \
+        --header="$header" \
+        --preview="echo {} | sed 's/.*\/  //' | sed 's/git log/git log --color=always/' | bash")
+
+    eval $(echo "$cmd" | sed 's/.*\/  //')
+}
+
+_async_fetch_all() {
+    echo
+    echo '  fetching everything ...'
+    echo
+    git fetch --all \
+                  && gg
+}
+
+gg() {
+    local command_list=$(
+        cat << EOF
+_async_fetch_all
+look_graph_log
+look_commit
+look_branch_or_tag
+EOF
+    )
+
+    local prompt="$PWD "
+    local header="take a look..."
+
+    eval $(
+           echo "$command_list" | fzf --reverse --padding 20% \
+             --prompt="$prompt" \
+             --header="$header"
+    )
+}
+
+export -f gg
