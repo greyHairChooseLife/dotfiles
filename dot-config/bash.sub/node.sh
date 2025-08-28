984d58c9fa21e0ad32ba73d423cb7c4c8e6c944c
sangyeon kim <greyhairchooselife@gmail.com>
2025-08-28 21:02:17 +0900

    lang: nodejs를 관리하는 파일을 분리

---
+++ dot-config/bash.sub/node.sh
@@ -0,0 +1,2 @@
+export NODE_BIN=$HOME/.npm-global/bin  # npm 기본
+export PATH=$PATH:$NODE_BIN
--- dot-config/bash.sub/path.sh
+++ dot-config/bash.sub/path.sh
@@ -1,6 +1,3 @@
-export PATH=$PATH:/opt/miniforge/bin  # miniforge 설치를 AUR에서 했다.
-export PATH=~/.npm-global/bin:$PATH   # npm 기본
-
 export GOPATH=$HOME/.go
 export GOBIN=$HOME/.go/bin
 export PATH=$PATH:$GOBIN
