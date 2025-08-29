7cc986ed90612574817bbb881508082652fe6f96
sangyeon kim <greyhairchooselife@gmail.com>
2025-08-29 11:15:07 +0900

    python: venv manager update for better life

--- dot-config/bash.sub/scripts/python/venv_manager.sh
+++ dot-config/bash.sub/scripts/python/venv_manager.sh
@@ -3,8 +3,14 @@
 export VENV_HOME="$HOME/.local/state/python_venv"
 [[ -d $VENV_HOME ]] || mkdir $VENV_HOME
 
+list_venv() {
+    ls -1 $VENV_HOME
+}
+
 activate_venv() {
-    selected=$(ls -1 $VENV_HOME | fzf)
+    selected=$(list_venv \
+               | fzf --header="Select a virtual environment to activate" \
+                     --preview="source $VENV_HOME/{}\/bin/activate && python --version && pip list")
 
     if [[ -z $selected ]]; then
         exit 0
@@ -17,7 +23,7 @@ activate_venv() {
 
 make_venv() {
     echo "Curently there are..."
-    ls -1 $VENV_HOME
+    list_venv
     echo
 
     read -p "New Venv Name: " name
@@ -26,16 +32,16 @@ make_venv() {
 }
 
 rm_venv() {
-    echo "Curently there are..."
-    ls -1 $VENV_HOME
-    echo
+    name=$(list_venv \
+                 | fzf --header="Select a virtual environment to remove" \
+                 --preview="du -sh $VENV_HOME/{}; echo; echo; stat $VENV_HOME/{}")
 
-    read -p "Remove Venv Name: " name
+    # read -p "Remove Venv Name: " name
     trash --interactive --recursive $VENV_HOME/$name
     echo "# Virtual environment removed: $name"
 }
 
-selected_command=$(printf "activate_venv\n make_venv\n rm_venv" | tr -d " " | fzf)
+selected_command=$(printf "activate_venv\n make_venv\n rm_venv" | tr -d " " | fzf --header="Choose an action")
 
 if [[ -z $selected_command ]]; then
     exit 0
