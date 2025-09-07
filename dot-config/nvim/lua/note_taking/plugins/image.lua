777fbc57ac957c679be9f3623026baa338b8a650
sangyeon kim <greyhairchooselife@gmail.com>
2025-09-07 19:19:08 +0900

    misc:

--- dot-config/nvim/lua/note_taking/plugins/image.lua
+++ dot-config/nvim/lua/note_taking/plugins/image.lua
@@ -1,42 +1,41 @@
 return {
-	"3rd/image.nvim",
-	enabled = require("utils").is_alacritty or false,
-	-- enabled = false,
-	dependencies = { "luarocks.nvim" },
-	lazy = false,
-	cond = function()
-		-- SSH 연결 여부 확인
-		local is_ssh = os.getenv("SSH_CLIENT") ~= nil
-			or os.getenv("SSH_TTY") ~= nil
-			or os.getenv("SSH_CONNECTION") ~= nil
+  "3rd/image.nvim",
+  enabled = require("utils").is_alacritty,
+  dependencies = { "luarocks.nvim" },
+  lazy = false,
+  cond = function()
+    -- SSH 연결 여부 확인
+    local is_ssh = os.getenv("SSH_CLIENT") ~= nil
+        or os.getenv("SSH_TTY") ~= nil
+        or os.getenv("SSH_CONNECTION") ~= nil
 
-		return not is_ssh -- SSH가 아닐 때만 플러그인 로드
-	end,
-	opts = {
-		-- backend = "kitty", -- kitty 터미널로 실행하면 매우 잘 된다. 크기 변경 등 더 매끄럽다.
-		backend = "ueberzug",
-		integrations = {
-			markdown = {
-				enabled = true,
-				clear_in_insert_mode = false,
-				download_remote_images = true,
-				only_render_image_at_cursor = false,
-				filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-			},
-		},
-		-- max_width = nil,
-		-- max_height = nil,
-		-- max_width_window_percentage = nil,
-		-- max_height_window_percentage = 80,
-		max_width = 100, -- tweak to preference
-		max_height = 12, -- ^
-		max_width_window_percentage = math.huge, -- this is necessary for a good experience
-		max_height_window_percentage = math.huge,
-		window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
-		window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-		editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
-		tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
-		hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" }, -- render image files as images when opened
-		scale_factor = 1.0,
-	},
+    return not is_ssh -- SSH가 아닐 때만 플러그인 로드
+  end,
+  opts = {
+    -- backend = "kitty", -- kitty 터미널로 실행하면 매우 잘 된다. 크기 변경 등 더 매끄럽다.
+    backend = "ueberzug",
+    integrations = {
+      markdown = {
+        enabled = true,
+        clear_in_insert_mode = false,
+        download_remote_images = true,
+        only_render_image_at_cursor = false,
+        filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+      },
+    },
+    -- max_width = nil,
+    -- max_height = nil,
+    -- max_width_window_percentage = nil,
+    -- max_height_window_percentage = 80,
+    max_width = 100,                                                          -- tweak to preference
+    max_height = 12,                                                          -- ^
+    max_width_window_percentage = math.huge,                                  -- this is necessary for a good experience
+    max_height_window_percentage = math.huge,
+    window_overlap_clear_enabled = true,                                      -- toggles images when windows are overlapped
+    window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+    editor_only_render_when_focused = false,                                  -- auto show/hide images when the editor gains/looses focus
+    tmux_show_only_in_active_window = false,                                  -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+    hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" }, -- render image files as images when opened
+    scale_factor = 1.0,
+  },
 }
--- dot-config/nvim/lua/qol/plugins/snacks-nvim.lua
+++ dot-config/nvim/lua/qol/plugins/snacks-nvim.lua
@@ -1,25 +1,25 @@
 return {
-	"folke/snacks.nvim",
-	priority = 1000,
-	commit = "f6c8e80dd724453a6d06e0af08dd4bb06f5505c0",
-	-- NOTE: to fix some bug, use this PR
-	--  https://github.com/folke/snacks.nvim/pull/1525
-	--  https://github.com/folke/snacks.nvim/issues/1524
-	lazy = false,
-	config = function()
-		require("snacks").setup({
-			image = { enabled = not require("utils").is_alacritty or true },
-			picker = require("picker.plugins.configs.snacks-picker"),
-			scroll = require("UI.plugins.configs.snacks-scroll"),
-			bigfile = { enabled = true },
-			styles = {
-				input = { -- https://github.com/folke/snacks.nvim/blob/main/docs/styles.md#input
-					border = "single",
-					row = vim.api.nvim_win_get_height(0) / 2 - 3,
-					relative = "editor", -- "cursor",
-					col = nil,
-				},
-			},
-		})
-	end,
+  "folke/snacks.nvim",
+  priority = 1000,
+  commit = "f6c8e80dd724453a6d06e0af08dd4bb06f5505c0",
+  -- NOTE: to fix some bug, use this PR
+  --  https://github.com/folke/snacks.nvim/pull/1525
+  --  https://github.com/folke/snacks.nvim/issues/1524
+  lazy = false,
+  config = function()
+    require("snacks").setup({
+      image = { enabled = not require("utils").is_alacritty },
+      picker = require("picker.plugins.configs.snacks-picker"),
+      scroll = require("UI.plugins.configs.snacks-scroll"),
+      bigfile = { enabled = true },
+      styles = {
+        input = { -- https://github.com/folke/snacks.nvim/blob/main/docs/styles.md#input
+          border = "single",
+          row = vim.api.nvim_win_get_height(0) / 2 - 3,
+          relative = "editor", -- "cursor",
+          col = nil,
+        },
+      },
+    })
+  end,
 }
