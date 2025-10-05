60297d66742501e926e9088f6eee1e5cd6798cf8
sangyeon kim <greyhairchooselife@gmail.com>
2025-10-05 13:08:05 +0900

    snip-config:

--- dot-config/nvim/lua/completion/modules/snippets.lua
+++ dot-config/nvim/lua/completion/modules/snippets.lua
@@ -1,16 +1,18 @@
 local ls = require("luasnip")
 local s = ls.snippet
-local t = ls.text_node
-local i = ls.insert_node
+local t = ls.text_node -- 걍 써지고 그만
+local i = ls.insert_node -- Tab 및 S-Tab으로 순회하며 입력
 local extras = require("luasnip.extras")
 local rep = extras.rep
 
 ls.add_snippets("c", {
+	-- MEMO:: main func
 	s("main", {
-		t("#include <stdio.h>\n\nint main() {\n    "),
+		t({ "#include <stdio.h>", "", "int main() {", "    " }),
 		i(1, "// code here"),
-		t("\n    return 0;\n}"),
+		t({ "", "    return 0;", "}" }),
 	}),
+	-- MEMO:: printf
 	s("printf", {
 		t('printf("'),
 		i(1, "%s"),
@@ -18,24 +20,26 @@ ls.add_snippets("c", {
 		i(2, "variable"),
 		t(");"),
 	}),
+	-- MEMO:: for
 	s("for", {
 		t("for (int "),
 		i(1, "i"),
 		t(" = 0; "),
-		rep(1), -- repeat the first insert node
+		rep(1),
 		t(" < "),
 		i(2, "n"),
 		t("; "),
 		rep(1),
-		t("++) {\n    "),
+		t({ ") {", "    " }),
 		i(3, "// loop body"),
-		t("\n}"),
+		t({ "", "}" }),
 	}),
+	-- MEMO:: if cond else
 	s("if", {
 		t("if ("),
 		i(1, "condition"),
-		t(") {\n    "),
+		t({ ") {", "    " }),
 		i(2, "// code"),
-		t("\n}"),
+		t({ "", "}" }),
 	}),
 })
