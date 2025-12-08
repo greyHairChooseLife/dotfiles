8d1be1920f0fbee60d60e745d5736dfb760fad4f
sangyeon kim <greyhairchooselife@gmail.com>
2025-12-08 11:12:57 +0900

    feat: git branch completion

---
+++ dot-config/bash.sub/git.sh
@@ -0,0 +1,15 @@
+source /usr/share/bash-completion/completions/git
+
+__git_complete ga _git_add
+__git_complete gco _git_commit
+__git_complete gch _git_checkout
+__git_complete gp _git_push
+__git_complete gt _git_tag
+__git_complete gfd _git_fetch
+__git_complete gb _git_branch
+__git_complete gba _git_branch
+__git_complete gs _git_status
+__git_complete gl _git_log
+__git_complete gst _git_stash
+__git_complete gf _git_fetch
+__git_complete gd _git_diff
