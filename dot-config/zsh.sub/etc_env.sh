83d8b29d05a9b14b9ea8a24b23003c86b53834f9
sangyeon kim <greyhairchooselife@gmail.com>
2026-07-12 23:07:49 +0900

    misc:

--- dot-config/zsh.sub/etc_env.sh
+++ dot-config/zsh.sub/etc_env.sh
@@ -1,28 +1,8 @@
-export EDITOR='nvim' # 충북프로메이커센터, 미래창업연구소
-# WARN:
-# ..
-# 1. 비밀번호 등 중요한 정보는 사용하지 않기(remote repository에 업로드 가능)
-
-export NAS_SERVER="/mnt/cb-nas" # 충북프로메이커센터, 미래창업연구소
-
+# WARN: 비밀번호 등 중요한 정보는 사용하지 않기(remote repository에 업로드 가능)
+export EDITOR='nvim'
 export MANPAGER='nvim +Man!'
 export MANWIDTH=999
-
 export LANG=en_US.UTF-8 # 이게 ssh servet-clinet간에 다르면 한글 랜더링 오류가 난다.
+export GTK_THEME=Adwaita:dark # for global theme
 
-# for global theme
-export GTK_THEME=Adwaita:dark
-
-# font
-# MEMO::
-# - kime는 ghostty에서 안된다. 도대체 왜!!!
-# - ibus는 버그가 많다. 아님 내가 설정을 못하는건가?
-export GTK_IM_MODULE=kime
-export QT_IM_MODULE=kime
-export XMODIFIERS=@im=kime
-# export GTK_IM_MODULE=ibus
-# export QT_IM_MODULE=ibus
-# export XMODIFIERS=@im=ibus
-
-# MEMO:: notes
 export ZK_NOTEBOOK_DIR="$HOME/Documents/zk"
